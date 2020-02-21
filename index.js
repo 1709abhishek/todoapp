21fe044f0e84d567565b7f84c72f8eee86608a1b
Abhishek Jain <1709abhishek@gmail.com>
2020-02-22 03:28:32 +0530

    created frontend

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
-
+app.use(express.static('./assets'));
 //use express router
 app.use('/', require('./routes'));
+
+//set up view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
 
 
 app.listen(port, function(err){
