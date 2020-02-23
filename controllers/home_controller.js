41ee7239b970f95f631ea2dfde5f2c97fa2d50dc
Abhishek Jain <1709abhishek@gmail.com>
2020-02-23 17:56:43 +0530

    add task storing in database done

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,11 @@
+const Task = require('../models/tasks');
 module.exports.home = function(req,res){
-    return res.render('home', {
-        title: "To-Do App"
+    Task.find({}, function(err, tasks){
+        if(err){console.log('error in fetching tasks from db');
+        return;}
+    return res.render('home',{
+        title: "To-Do list",
+        task_list : tasks
     });
+});
 }
--- index.js
+++ index.js
@@ -11,6 +11,27 @@ app.use('/', require('./routes'));
 //set up view engine
 app.set('view engine', 'ejs');
 app.set('views', './views');
+app.use(express.urlencoded());
+
+
+app.post('/create-task', function(req,res){
+    // contactList.push({
+    //     name: req.body.name,
+    //     phone: req.body.phone
+    // });
+    // contactList.push(req.body);
+    Task.create({
+        description: req.body.description,
+        date: req.body.date,
+        category: req.body.category
+    }, function(err, newTask){
+        if(err){console.log('error in creating a contact');
+        return;}
+
+        console.log('******', newTask);
+        return res.redirect('back');
+    });
+});
 
 
 app.listen(port, function(err){
